960e3f8e49961e5401624eaba7d4586e20bbf548
Bibudh Lahiri <bibudhlahiri@gmail.com>
2014-01-22 16:37:53 -0500

    Added method prepare_data_page_sequence, changed prepare_data and other methods to handle 50 best 2-grams

--- behav_features.R
+++ behav_features.R
@@ -4,26 +4,26 @@ library(e1071)
 library(nnet)
 library(rpart)
 
-prepare_data <- function()
+prepare_data_user_behavior <- function()
 {
   con <- dbConnect(PostgreSQL(), user="postgres", password = "impetus123",  
                    host = "localhost", port="5432", dbname = "cleartrail")
-  statement <- "select bs.ClientIPServerIP, bs.browsingsessionid, bs.MarkedCategory, max(hr.timestamp) - min(hr.timestamp) as duration_seconds
+  statement <- "select bs.id, bs.MarkedCategory, max(hr.timestamp) - min(hr.timestamp) as duration_seconds
                 from browsing_sessions bs, http_requests hr
                 where bs.ClientIPServerIP = hr.ClientIPServerIP
                 and bs.browsingsessionid = hr.browsingsessionid
                 and bs.MarkedCategory in ('User', 'Bot')
-                group by bs.ClientIPServerIP, bs.browsingsessionid, bs.MarkedCategory
-                order by bs.ClientIPServerIP, bs.browsingsessionid"
+                group by bs.id, bs.MarkedCategory
+                order by bs.id"
   res <- dbSendQuery(con, statement)
   sess_durations <- fetch(res, n = -1) 
 
-  statement <- "select bs.ClientIPServerIP, bs.browsingsessionid, count(*) n_pages_from_session
+  statement <- "select bs.id, count(*) n_pages_from_session
                 from browsing_sessions bs, http_requests_pages hr
                 where bs.ClientIPServerIP = hr.ClientIPServerIP
                 and bs.browsingsessionid = hr.browsingsessionid
                 and bs.MarkedCategory in ('User', 'Bot')
-                group by bs.ClientIPServerIP, bs.browsingsessionid"
+                group by bs.id"
   res <- dbSendQuery(con, statement)
   sess_pages <- fetch(res, n = -1)
   
@@ -31,12 +31,12 @@ prepare_data <- function()
   sessions <- merge(sess_durations, sess_pages, all.x = TRUE) 
 
 
-  statement <- "select bs.ClientIPServerIP, bs.browsingsessionid, count(distinct hr.completeurl) n_distinct_pages_from_session
+  statement <- "select bs.id, count(distinct hr.completeurl) n_distinct_pages_from_session
                 from browsing_sessions bs, http_requests_pages hr
                 where bs.ClientIPServerIP = hr.ClientIPServerIP
                 and bs.browsingsessionid = hr.browsingsessionid
                 and bs.MarkedCategory in ('User', 'Bot')
-                group by bs.ClientIPServerIP, bs.browsingsessionid
+                group by bs.id
                 order by n_distinct_pages_from_session"
   res <- dbSendQuery(con, statement)
   sess_distinct_pages <- fetch(res, n = -1)
@@ -46,8 +46,73 @@ prepare_data <- function()
   #print(sessions)
   sessions[,"markedcategory"] <- as.factor(sessions[,"markedcategory"])
   dbDisconnect(con)
+  cat(paste("nrow(sessions) = ", nrow(sessions), "\n", sep = ""))
   return(sessions)
 }
+
+#Get the frequencies of 2-grams of pages for the (539) sessions which visit 2 or more pages
+prepare_data_page_sequence <- function()
+ {
+  con <- dbConnect(PostgreSQL(), user="postgres", password = "impetus123",  
+                   host = "localhost", port="5432", dbname = "cleartrail")
+ 
+  #bs.id is the concatenation of bs.ClientIPServerIP and bs.browsingsessionid
+  statement <- paste("select bs.id, tg.gram_sequence, tg.frequency
+                      from browsing_sessions bs inner join two_grams_with_pages tg 
+                      on (bs.ClientIPServerIP = tg.ClientIPServerIP
+                      and bs.BrowsingSessionID = tg.BrowsingSessionID)
+                      where bs.markedcategory in ('User', 'Bot')
+                      order by bs.id, tg.gram_sequence", sep = "")
+  res <- dbSendQuery(con, statement)
+  data <- fetch(res, n = -1)
+  n_data <- nrow(data)
+
+  nrows <- length(unique(data$id))
+  ncols <- length(unique(data$gram_sequence))
+  data_matrix <- mat.or.vec(nrows, ncols)
+  rownames(data_matrix) <- unique(data$id)
+  colnames(data_matrix) <- unique(data$gram_sequence)
+  
+  for (i in 1:n_data)
+  {
+    data_matrix[data[i, "id"], data[i, "gram_sequence"]] <- data[i, "frequency"]
+  }
+  cat(paste("nrow(data_matrix) = ", nrow(data_matrix), ", ncol(data_matrix) = ", ncol(data_matrix), "\n", sep = ""))
+  #print(data_matrix)
+  dbDisconnect(con) 
+  return(data.frame(data_matrix))
+}
+
+create_table_script_for_page_sequence_matrix <- function()
+{
+  page_sequence <- prepare_data_page_sequence()
+  columns <- colnames(page_sequence)
+  statement <- "create table page_sequence_matrix (session_id varchar, "
+  for (column in columns)
+  {
+    statement <- paste(statement, column, " integer, " , sep = "") 
+  }
+  statement <- substring(statement, 1, nchar(statement) - 2)
+  statement <- paste(statement, ")", sep = "")
+  sink("create_table_script_for_page_sequence_matrix.sql")
+  cat(statement)
+  sink()
+}
+
+
+prepare_data <- function()
+{
+  #page_sequence <- prepare_data_page_sequence()
+  page_sequence <- read.csv("/Users/blahiri/hiddenmarkovmodel/documents/prepared_data_post_feature_selection.csv")
+  user_behavior <- prepare_data_user_behavior()
+
+  sessions <- merge(user_behavior, page_sequence, all.x = TRUE)
+  sessions[is.na(sessions)] <- 0
+  cat(paste("nrow(sessions) = ", nrow(sessions), ", ncol(sessions) = ", ncol(sessions), "\n", sep = ""))
+  #print(colnames(sessions))
+  sessions
+}
+
 
 
 #Classification with simple attributes like number of pages visited in session, duration of session, etc
@@ -58,7 +123,7 @@ classify_rf <- function()
   #Random forest does not need splitting data into training and test sets, or cross-validation, as the prediction on a point 
   #is based on trees (about 1/3rd) where it is out-of-bag. The trees differ in two things: each tree takes a bootstrap sample of 
   #the training data, and picks mtry features at random to build the tree.
-  sessions.rf <- randomForest(sessions[, c("duration_seconds", "n_pages_from_session", "n_distinct_pages_from_session")],  
+  sessions.rf <- randomForest(sessions[,!(names(sessions) %in% c("id", "markedcategory"))], 
                               sessions[,"markedcategory"], 
                               prox = TRUE)
 
@@ -75,8 +140,11 @@ train_validate_test_rf <- function()
    test = (-train)
    cat(paste("Size of training data = ", length(train), ", size of test data = ", (nrow(sessions) - length(train)), "\n", sep = ""))
 
-   tune.out = tune.randomForest(markedcategory ~ duration_seconds + n_pages_from_session + n_distinct_pages_from_session, 
-                                data = sessions[train, ], ntree = seq(100, 600, 100), mtry = c(1, 2))
+   #tune.out = tune.randomForest(markedcategory ~ duration_seconds + n_pages_from_session + n_distinct_pages_from_session, 
+   #                             data = sessions[train, ], ntree = seq(100, 600, 100), mtry = c(1, 2))
+   tune.out = tune.randomForest(sessions[,!(names(sessions) %in% c("id", "markedcategory"))], 
+                                sessions[,"markedcategory"], 
+                                ntree = seq(100, 600, 100), mtry = c(1, 2))
    bestmod <- tune.out$best.model
    ypred <- predict(bestmod, newdata = sessions[test, ], type = "class")
    print(table(sessions[test, "markedcategory"], ypred, dnn = list('actual', 'predicted')))
@@ -88,7 +156,8 @@ train_validate_test_svm <- function()
 {
   set.seed(1)
   sessions <- prepare_data()
-  x <- sessions[, c("duration_seconds", "n_pages_from_session", "n_distinct_pages_from_session")]
+  #x <- sessions[, c("duration_seconds", "n_pages_from_session", "n_distinct_pages_from_session")]
+  x <- sessions[,!(names(sessions) %in% c("id", "markedcategory"))]
   y <- sessions[,"markedcategory"]
   train = sample(1:nrow(x), 0.5*nrow(x))
   test = (-train)
@@ -98,13 +167,12 @@ train_validate_test_svm <- function()
   bot_class_weight <- as.numeric(tab["User"]/tab["Bot"])
   cat(paste("bot_class_weight = ", bot_class_weight, "\n", sep = ""))
  
-  #tune.out = tune.svm(x[train, ], y[train], kernel = "linear", 
-  #                    class.weights = c(Bot = bot_class_weight) , cost = c(0.001, 0.01, 0.1, 1, 5, 10, 100))
-  tune.out = tune.svm(x[train, ], y[train], kernel = "radial", 
-                      class.weights = c(Bot = bot_class_weight), 
-                      cost = c(0.001, 0.01, 0.1, 1, 10, 100, 1000), gamma = c(0.125, 0.25, 0.5, 1, 2, 3, 4, 5)
-                      )
-  #tune.out = tune.svm(x[train, ], y[train], kernel = "polynomial", cost = c(0.001, 0.01, 0.1, 1, 5, 10, 100), degree = c(2, 3, 4))
+  tune.out = tune.svm(x[train, ], y[train], kernel = "linear", 
+                      class.weights = c(Bot = bot_class_weight) , cost = c(0.001, 0.01, 0.1, 1, 5, 10, 100))
+  #tune.out = tune.svm(x[train, ], y[train], kernel = "radial", 
+  #                    class.weights = c(Bot = bot_class_weight), 
+  #                    cost = c(0.001, 0.01, 0.1, 1, 10, 100, 1000), gamma = c(0.125, 0.25, 0.5, 1, 2, 3, 4, 5)
+  #                    )
   bestmod <- tune.out$best.model
 
   ypred = predict(bestmod, x[train, ])
@@ -121,7 +189,8 @@ train_validate_test_svm <- function()
  {
    set.seed(1)
    sessions <- prepare_data()
-   x <- sessions[, c("duration_seconds", "n_pages_from_session", "n_distinct_pages_from_session")]
+   #x <- sessions[, c("duration_seconds", "n_pages_from_session", "n_distinct_pages_from_session")]
+   x <- sessions[,!(names(sessions) %in% c("id", "markedcategory"))]
    y <- sessions[,"markedcategory"]
    train = sample(1:nrow(x), 0.5*nrow(x))
    test = (-train)
@@ -143,7 +212,19 @@ train_validate_test_rpart <- function()
    test = (-train)
    cat(paste("Size of training data = ", length(train), ", size of test data = ", (nrow(sessions) - length(train)), "\n", sep = ""))
 
-   tune.out = tune.rpart(markedcategory ~ duration_seconds + n_pages_from_session + n_distinct_pages_from_session, data = sessions[train, ], minsplit = c(5, 10, 15), maxdepth = c(1, 3, 5, 7))
+   #tune.out = tune.rpart(markedcategory ~ duration_seconds + n_pages_from_session + n_distinct_pages_from_session, data = sessions[train, ], minsplit = c(5, 10, 15), maxdepth = c(1, 3, 5, 7))
+   str_formula <- "markedcategory ~ "
+   for (column in colnames(sessions))
+   {
+     if (column != 'id' & column != 'markedcategory')
+     {
+       str_formula <- paste(str_formula, column, " + ", sep = "")
+     }
+   }
+   str_formula <- substring(str_formula, 1, nchar(str_formula) - 2)
+   #print(str_formula)
+   
+   tune.out = tune.rpart(as.formula(str_formula), data = sessions[train, ], minsplit = c(5, 10, 15), maxdepth = c(1, 3, 5, 7))
    bestmod <- tune.out$best.model
    ypred <- predict(bestmod, newdata = sessions[test, ], type = "class")
    print(table(sessions[test, "markedcategory"], ypred, dnn = list('actual', 'predicted')))
@@ -154,14 +235,25 @@ train_validate_test_nn_single_hidden_layer <- function()
  {
    set.seed(1)
    sessions <- prepare_data()
-   x <- sessions[, c("duration_seconds", "n_pages_from_session", "n_distinct_pages_from_session")]
+   #x <- sessions[, c("duration_seconds", "n_pages_from_session", "n_distinct_pages_from_session")]
+   x <- sessions[,!(names(sessions) %in% c("id", "markedcategory"))]
    y <- sessions[,"markedcategory"]
    train = sample(1:nrow(x), 0.5*nrow(x))
    test = (-train)
    y.test = y[test]
    cat(paste("Size of training data = ", length(train), ", size of test data = ", (nrow(sessions) - length(train)), "\n", sep = ""))
 
-   tune.out = tune.nnet(markedcategory ~ duration_seconds + n_pages_from_session + n_distinct_pages_from_session, data = sessions[train, ], size = seq(2, 12, 2)) 
+   #tune.out = tune.nnet(markedcategory ~ duration_seconds + n_pages_from_session + n_distinct_pages_from_session, data = sessions[train, ], size = seq(2, 12, 2)) 
+   str_formula <- "markedcategory ~ "
+   for (column in colnames(sessions))
+   {
+     if (column != 'id' & column != 'markedcategory')
+     {
+       str_formula <- paste(str_formula, column, " + ", sep = "")
+     }
+   }
+   str_formula <- substring(str_formula, 1, nchar(str_formula) - 2)
+   tune.out = tune.nnet(as.formula(str_formula), data = sessions[train, ], size = seq(2, 12, 2))
    
    bestmod <- tune.out$best.model
    ypred <- predict(bestmod, newdata = sessions[test, ], type = "class")
